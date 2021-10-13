2cb97fd2c67c20e6509087b8f747daf37b5150a6
chirv <chirv@DESKTOP-VOT2QOU>
2021-10-13 19:29:31 +0200

    Добавьте файлы проекта.

---
+++ Lab2(2)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab2_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стоимость");
+            double a = double.Parse(Console.ReadLine());
+            if (a > 1000 && a < 2000) a = a - a * 0.05;
+            else if (a > 2000 && a < 5000) a = a - a * 0.1;
+            Console.WriteLine($"Цена с учетом скидки: {a}");
+        }
+    }
+}
